39666f71286fa36193bd8c338f20da772898c716
gonza <gonzagiacomino18@gmail.com>
2024-11-18 14:04:07 -0300

    Haciendo JWT

---
+++ microservice.gateway/src/main/java/com/microservice/gateway/repository/AuthorityRepository.java
@@ -0,0 +1,10 @@
+package com.microservice.gateway.repository;
+
+import com.microservice.gateway.entity.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+
+}
---
+++ microservice.gateway/src/main/java/com/microservice/gateway/security/AuthotityConstant.java
@@ -0,0 +1,10 @@
+package com.microservice.gateway.security;
+
+public final class AuthotityConstant {
+
+    private AuthotityConstant() {}
+
+    public static final String _ADMIN = "ADMIN";
+    public static final String _PROFESOR = "PROFESOR";
+    public static final String _ALUMNO = "ALUMNO";
+}
